0dba23895f3ed1c7b5957400c7a283a87ab2ba9b
Yuty-Ya <trooperskid214@gmail.com>
2026-03-31 22:38:03 -0500

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,40 @@
+// hi.
+
+"use strict";
+
+let month = prompt("what is your favoite month?");
+month = month.toLowerCase();
+
+switch(month) {
+    case"march":
+    case"april":
+    case"may":
+    console.log("Spring is nice with everything blooming.");
+    break;
+    // getting roblox lua vibes. ._ .
+
+    case"june":
+    case"july":
+    case"augest":
+    console.log("You enjoy the summer months!");
+    break;
+
+    case"september":
+    case"october":
+    case"november":
+    console.log("Fall is fun with all of the pretty colors.")
+    break;
+
+    case"december":
+    case"january":
+    case"february":
+    console.log("You love the winter months!")
+    break;
+
+
+    default:
+    console.log("Other months are interseting too! (Or a invaild answer)")
+    break;
+}
+
+//OK I THINK I DIDN'T FORGET ANY SEMI COLORS.
