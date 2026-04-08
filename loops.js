e829f0b881143804a0f2358d7d9ce6d054732ed3
Yuty-Ya <trooperskid214@gmail.com>
2026-04-07 22:49:57 -0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,14 @@
+// jon g 4/7/26
+// I dont knopw where we are meant to put the answer for question 1, But it is 5 times.
+
+"use strict";
+
+for(let Crate = 1; Crate <= 20; Crate++) {
+    if (Crate % 5 == 0) {
+        console.log("Handle with care")
+    } else if (Crate % 2 == 0) {
+        console.log("Load crate")
+    } else {
+        console.log("Inspect crate, Unknown Crate/Cargo")
+    }
+}
